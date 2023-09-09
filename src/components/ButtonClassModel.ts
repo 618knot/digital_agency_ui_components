2aebeba91eb41333642314548a92a7488fafd522
618knot <knot618@gmail.com>
2023-09-09 18:56:06 +0900

    ボタンコンポーネントの実装

---
+++ src/components/ButtonClassModel.ts
@@ -0,0 +1,14 @@
+export const enum ButtonClassModel {
+    PrimaryLarge = "primary large",
+    PrimaryMedium = "primary medium",
+    PrimarySmall = "primary small",
+    PrimaryXSmall = "primary x-small",
+    SecondaryLarge = "secondary large",
+    SecondaryMedium = "secondary medium",
+    SecondarySmall = "secondary small",
+    SecondaryXSmall = "secondary x-small",
+    TertiaryLarge = "tertiary large",
+    TertiaryMedium = "tertiary medium",
+    TertiarySmall = "tertiary small",
+    TertiaryXSmall = "tertiary x-small",
+}
--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -1,8 +1,18 @@
-export const Button = () => {
+import './Button.scss';
+
+type ButtonProps = {
+    label: string;
+    className: string;
+    onClick?: () => void;
+};
+
+export const Button = (props :ButtonProps) => {
     return (
         <button
-            className={}
-            onClick={}
-        />
+            className={props.className}
+            onClick={props.onClick}
+        >
+            {props.label}
+        </button>
     );
 }
